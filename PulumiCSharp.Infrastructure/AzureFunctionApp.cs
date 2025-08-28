4ec0cc0c49d7a00576f3eb99af9c20b13c1f50f9
Daniel Ward <dward256@hotmail.com>
2025-08-28 09:05:16 -0500

    component resource stuff

---
+++ PulumiCSharp.Infrastructure/AzureFunctionApp.cs
@@ -0,0 +1,166 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Insights;
+using Pulumi.AzureNative.OperationalInsights;
+using Pulumi.AzureNative.OperationalInsights.Inputs;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Web;
+using Pulumi.AzureNative.Web.Inputs;
+
+namespace PulumiCSharp.Infrastructure;
+
+public class DotNetVersion : ResourceArgs
+{
+    public static readonly DotNetVersion V8 = new DotNetVersion
+    {
+        NetFrameworkVersion = "v8.0",
+        LinuxFxVersion = "DOTNET-ISOLATED|8.0"
+    };
+    public static readonly DotNetVersion V9 = new DotNetVersion
+    {
+        NetFrameworkVersion = "v9.0",
+        LinuxFxVersion = "DOTNET-ISOLATED|9.0"
+    };
+
+    [Input("netFrameworkVersion", true)] public required Input<string> NetFrameworkVersion { get; set; }
+    [Input("linuxFxVersion", true)] public required Input<string> LinuxFxVersion { get; set; }
+}
+
+public class AzureFunctionAppArgs : ResourceArgs
+{
+    [Input("resourceGroupName", true)] public required Input<string> ResourceGroupName { get; set; }
+    [Input("storageAccountName", true)] public required Input<string> StorageAccountName { get; set; }
+    [Input("functionName", true)] public required Input<string> FunctionName { get; set; }
+    [Input("publishPath", true)] public required Input<string> PublishPath { get; set; }
+    [Input("dotNetVersion", true)] public required Input<DotNetVersion> DotNetVersion { get; set; }
+}
+
+public class AzureFunctionApp : ComponentResource
+{
+    [Output] public Output<string> ApiUrl { get; set; }
+
+    public AzureFunctionApp(string name, AzureFunctionAppArgs args, ComponentResourceOptions? options = null)
+    : base("danielwarddev:PulumiCSharp:AzureFunctionApp", name, args, options)
+    {
+        // Create a new blob container and blob to hold the ZIP file of the packaged app for the Azure Function
+        var blobContainer = new BlobContainer($"{name}-function-zip-container", new BlobContainerArgs
+        {
+            ResourceGroupName = args.ResourceGroupName,
+            AccountName = args.StorageAccountName,
+            PublicAccess = PublicAccess.None
+        },
+        new() { Parent = this });
+
+        var blob = new Blob($"{name}-functionZipBlob", new BlobArgs
+        {
+            ResourceGroupName = args.ResourceGroupName,
+            AccountName = args.StorageAccountName,
+            ContainerName = blobContainer.Name,
+            Type = BlobType.Block,
+            Source = args.PublishPath.Apply(path => new FileArchive(path) as AssetOrArchive) // This will create a .zip for us from the folder
+        },
+        new() { Parent = this });
+
+        // Get the signed URL for the blob by calling ListStorageAccountServiceSAS on Azure
+        var sasToken = ListStorageAccountServiceSAS.Invoke(new ListStorageAccountServiceSASInvokeArgs
+        {
+            ResourceGroupName = args.ResourceGroupName,
+            AccountName = args.StorageAccountName,
+            Protocols = HttpProtocol.Https,
+            SharedAccessStartTime = "2022-01-01",
+            SharedAccessExpiryTime = "2030-01-01",
+            Resource = SignedResource.C, // Container
+            Permissions = Permissions.R, // Read
+            CanonicalizedResource = Output.Format($"/blob/{args.StorageAccountName}/{blobContainer.Name}")
+        },
+        new() { Parent = this })
+            .Apply(result => result.ServiceSasToken);
+
+        var codeBlobUrl = Output.Format($"https://{args.StorageAccountName}.blob.core.windows.net/{blobContainer.Name}/{blob.Name}?{sasToken}");
+
+        // Create an Azure workspace and Application Insights inside of it
+        var workspace = new Workspace($"{name}-workspace", new()
+        {
+            ResourceGroupName = args.ResourceGroupName,
+            RetentionInDays = 30,
+            Sku = new WorkspaceSkuArgs
+            {
+                Name = "PerGB2018",
+            },
+            Features = new WorkspaceFeaturesArgs
+            {
+                EnableDataExport = true
+            }
+        },
+        new() { Parent = this });
+
+        var appInsights = new Component($"{name}-appInsights", new()
+        {
+            ResourceGroupName = args.ResourceGroupName,
+            ApplicationType = "web",
+            Kind = "web",
+            WorkspaceResourceId = workspace.Id
+        },
+        new() { Parent = this });
+
+        var appServicePlan = new AppServicePlan($"{name}-appServicePlan", new AppServicePlanArgs
+        {
+            ResourceGroupName = args.ResourceGroupName,
+            Kind = "Linux",
+            Sku = new SkuDescriptionArgs
+            {
+                Name = "Y1",
+                Tier = "Dynamic"
+            },
+            Reserved = true,
+            Location = "WestUS"
+        },
+        new() { Parent = this });
+
+        // Use everything above to create the app
+        var app = new WebApp($"{name}-my-function-app", new()
+        {
+            ResourceGroupName = args.ResourceGroupName,
+            ServerFarmId = appServicePlan.Id,
+            Kind = "FunctionApp",
+            SiteConfig = new SiteConfigArgs
+            {
+                NetFrameworkVersion = args.DotNetVersion.Apply(x => x.NetFrameworkVersion),
+                LinuxFxVersion = args.DotNetVersion.Apply(x => x.LinuxFxVersion),
+                DetailedErrorLoggingEnabled = true,
+                HttpLoggingEnabled = true,
+                AppSettings = new[]
+                {
+                    new NameValuePairArgs
+                    {
+                        Name = "FUNCTIONS_WORKER_RUNTIME",
+                        Value = "dotnet-isolated",
+                    },
+                    new NameValuePairArgs
+                    {
+                        Name = "FUNCTIONS_EXTENSION_VERSION",
+                        Value = "~4",
+                    },
+                    new NameValuePairArgs
+                    {
+                        Name = "WEBSITE_RUN_FROM_PACKAGE",
+                        Value = codeBlobUrl,
+                    },
+                    new NameValuePairArgs
+                    {
+                        Name = "APPINSIGHTS_INSTRUMENTATIONKEY",
+                        Value = appInsights.InstrumentationKey
+                    }
+                },
+                Cors = new CorsSettingsArgs
+                {
+                    AllowedOrigins = new[] { "*" }
+                },
+            },
+        },
+        new() { Parent = this });
+
+        ApiUrl = Output.Format($"https://{app.DefaultHostName}/api/{args.FunctionName}");
+
+        RegisterOutputs();
+    }
+}
--- PulumiCSharp.Infrastructure/Program.cs
+++ PulumiCSharp.Infrastructure/Program.cs
@@ -1,21 +1,22 @@
 ﻿using System;
-using Pulumi;
 using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Storage;
 using Pulumi.AzureNative.Storage.Inputs;
 using System.Collections.Generic;
 using System.IO;
-using Pulumi.AzureNative.Insights;
-using Pulumi.AzureNative.OperationalInsights;
-using Pulumi.AzureNative.OperationalInsights.Inputs;
-using Pulumi.AzureNative.Web;
-using Pulumi.AzureNative.Web.Inputs;
 using Pulumi.Command.Local;
 using Kind = Pulumi.AzureNative.Storage.Kind;
 using StorageAccountArgs = Pulumi.AzureNative.Storage.StorageAccountArgs;
+using PulumiCSharp.Infrastructure;
 
-return await Pulumi.Deployment.RunAsync(() =>
+return await Pulumi.Deployment.RunAsync(async () =>
 {
+    // To debug the infra project, uncomment this code, run pulumi up, then attach to the process in your IDE
+    /*while (!Debugger.IsAttached)
+    {
+        await System.Threading.Tasks.Task.Delay(1000);
+    }*/
+
     var resourceGroup = new ResourceGroup("resourceGroup");
     
     var storageAccount = new StorageAccount("sa", new StorageAccountArgs
@@ -27,157 +28,44 @@ return await Pulumi.Deployment.RunAsync(() =>
         },
         Kind = Kind.StorageV2
     });
-    
-    var appServicePlan = new AppServicePlan("appServicePlan", new AppServicePlanArgs
-    {
-        ResourceGroupName = resourceGroup.Name,
-        Kind = "Linux",
-        Sku = new SkuDescriptionArgs
-        {
-            Name = "Y1",
-            Tier = "Dynamic"
-        },
-        Reserved = true,
-        Location = "WestUS"
-    });
-    
-    // Create a new blob container and blob to hold the ZIP file of the packaged app for the Azure Function
-    var blobContainer = new BlobContainer("function-zip-container", new BlobContainerArgs
-    {
-        ResourceGroupName = resourceGroup.Name,
-        AccountName = storageAccount.Name,
-        PublicAccess = PublicAccess.None
-    });
 
     var publishCommand = Run.Invoke(new()
     {
-        Command = "dotnet publish --output publish",
+        Command = $"dotnet clean && dotnet publish --output publish",
         Dir = Path.GetFullPath(Path.Combine("..", "PulumiCSharp.ConsoleApp")),
         Environment = new Dictionary<string, string>
         {
             ["DOTNET_CLI_TELEMETRY_OPTOUT"] = "1"
         }
     });
+
     publishCommand.Apply(x =>
     {
         Console.WriteLine(x.Stdout);
         return x; // We don't care about this, but Apply<T>() forces you to return a T
     });
-    
-    var blob = new Blob("functionZipBlob", new BlobArgs
+
+    var functionApp1 = new AzureFunctionApp("cool-function-1", new()
     {
         ResourceGroupName = resourceGroup.Name,
-        AccountName = storageAccount.Name,
-        ContainerName = blobContainer.Name,
-        Type = BlobType.Block,
-        Source = new FileArchive("../PulumiCSharp.ConsoleApp/publish") // This will create a .zip for us from the folder
+        StorageAccountName = storageAccount.Name,
+        FunctionName = "MyCoolFunction",
+        PublishPath = "../PulumiCSharp.ConsoleApp/publish",
+        DotNetVersion = DotNetVersion.V8
     });
 
-    // Get the signed URL for the blob by calling ListStorageAccountServiceSAS on Azure
-    var sasToken = ListStorageAccountServiceSAS.Invoke(new ListStorageAccountServiceSASInvokeArgs
+    var functionApp2 = new AzureFunctionApp("cool-function-2", new()
     {
         ResourceGroupName = resourceGroup.Name,
-        AccountName = storageAccount.Name,
-        Protocols = HttpProtocol.Https,
-        SharedAccessStartTime = "2022-01-01",
-        SharedAccessExpiryTime = "2030-01-01",
-        Resource = SignedResource.C, // Container
-        Permissions = Permissions.R, // Read
-        CanonicalizedResource = Output.Format($"/blob/{storageAccount.Name}/{blobContainer.Name}")
-    }).Apply(result => result.ServiceSasToken);
-    
-    var codeBlobUrl = Output.Format($"https://{storageAccount.Name}.blob.core.windows.net/{blobContainer.Name}/{blob.Name}?{sasToken}");
-
-    // Create an Azure workspace and Application Insights inside of it
-    var workspace = new Workspace("workspace", new()
-    {
-        ResourceGroupName = resourceGroup.Name,
-        RetentionInDays = 30,
-        Sku = new WorkspaceSkuArgs
-        {
-            Name = "PerGB2018",
-        },
-        Features = new WorkspaceFeaturesArgs
-        {
-            EnableDataExport = true
-        }
+        StorageAccountName = storageAccount.Name,
+        FunctionName = "MyCoolFunction",
+        PublishPath = "../PulumiCSharp.ConsoleApp/publish",
+        DotNetVersion = DotNetVersion.V8
     });
 
-    var appInsights = new Component("appInsights", new()
-    {
-        ResourceGroupName = resourceGroup.Name,
-        ApplicationType = "web",
-        Kind = "web",
-        WorkspaceResourceId = workspace.Id
-    });
-
-    // Use everything above to create the app
-    var app = new WebApp("my-function-app", new()
-    {
-        ResourceGroupName = resourceGroup.Name,
-        ServerFarmId = appServicePlan.Id,
-        Kind = "FunctionApp",
-        SiteConfig = new SiteConfigArgs
-        {
-            NetFrameworkVersion = "v8.0",
-            LinuxFxVersion = "DOTNET-ISOLATED|8.0",
-            DetailedErrorLoggingEnabled = true,
-            HttpLoggingEnabled = true,
-            AppSettings = new[]
-            {
-                new NameValuePairArgs
-                {
-                    Name = "FUNCTIONS_WORKER_RUNTIME",
-                    Value = "dotnet-isolated",
-                },
-                new NameValuePairArgs
-                {
-                    Name = "FUNCTIONS_EXTENSION_VERSION",
-                    Value = "~4",
-                },
-                new NameValuePairArgs
-                {
-                    Name = "WEBSITE_RUN_FROM_PACKAGE",
-                    Value = codeBlobUrl,
-                },
-                new NameValuePairArgs
-                {
-                    Name = "APPINSIGHTS_INSTRUMENTATIONKEY",
-                    Value = appInsights.InstrumentationKey
-                }
-            },
-            Cors = new CorsSettingsArgs
-            {
-                AllowedOrigins = new[]
-                {
-                    "*",
-                },
-            },
-        },
-    });
-    
-    var storageAccountKeys = ListStorageAccountKeys.Invoke(new ListStorageAccountKeysInvokeArgs
-    {
-        ResourceGroupName = resourceGroup.Name,
-        AccountName = storageAccount.Name
-    });
-
-    var primaryStorageKey = storageAccountKeys.Apply(accountKeys =>
-    {
-        var firstKey = accountKeys.Keys[0].Value;
-        return Output.CreateSecret(firstKey);
-    });
-    
     return new Dictionary<string, object?>
     {
-        ["primaryStorageKey"] = primaryStorageKey,
-        ["apiUrl"] = app.DefaultHostName.Apply(x => $"https://{x}/api/MyCoolFunction"),
-        // ["publishOutput"] = publishCommand.Stdout.Apply(x => x)
-        /*["publishOutput"] = publishCommand.Stdout.Apply(output => 
-        {
-            if (string.IsNullOrEmpty(output)) return "No output";
-            var lines = output.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-            return string.Join(Environment.NewLine, lines.Select(line => $"  {line.Trim()}"));
-        })*/
+        ["coolApp1-apiUrl"] = functionApp1.ApiUrl,
+        ["coolApp2-apiUrl"] = functionApp2.ApiUrl
     };
 });
